0d1f3ac21b9e1a83f50a294c86a174f62ecaf75d
Antolina Gonzalez <antolina487@gmail.com>
2020-06-13 23:32:04 -0300

    heroku

--- proyecto/django_init/django_init/wsgi.py
+++ proyecto/django_init/django_init/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_init.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_init.settings.production')
 
 application = get_wsgi_application()
